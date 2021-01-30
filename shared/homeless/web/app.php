cff3991d6855a50a3cb5fd2d3a3c62637465a2e5
Yaroslav <igogorevi4@users.noreply.github.com>
2021-01-30 23:18:08 +0200

    Revert "room configuration"

--- shared/homeless/web/app.php
+++ shared/homeless/web/app.php
@@ -9,7 +9,6 @@ $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../app/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
-// $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
