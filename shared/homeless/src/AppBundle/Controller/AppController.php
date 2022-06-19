707827835a4aaf081b5f6ff826b78c80222ab027
Semyon.Atamas <semyon.atamas@jetbrains.com>
2022-06-19 23:35:50 +0400

    Minor: remove deprecated annotation usage

--- shared/homeless/src/AppBundle/Controller/AppController.php
+++ shared/homeless/src/AppBundle/Controller/AppController.php
@@ -5,10 +5,10 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\ClientField;
 use AppBundle\Entity\ContractStatus;
 use Application\Sonata\UserBundle\Entity\User;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/app")
