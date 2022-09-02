ce5063011dbefdc6f2cb8cdc1fb042fe7fb88e6f
Alexander <kvendingoldo@yandex.ru>
2022-09-02 16:28:13 +0400

    commit changes

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # Run middleware pipeline
-ansible-galaxy install -r requirements.yaml
-ansible-playbook playbook.yaml -e"@infra.yaml" -e"@values.yaml"
+#ansible-galaxy install -r requirements.yaml
+#ansible-playbook playbook.yaml -e"@infra.yaml" -e"@values.yaml"
+
+
+ansible-galaxy install  geerlingguy.docker
+ansible-playbook playbook.yaml
