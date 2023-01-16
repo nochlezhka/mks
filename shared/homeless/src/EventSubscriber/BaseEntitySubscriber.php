9c1027554149a719c96c3084a66da343eed1deb8
Semyon.Atamas <semyon.atamas@jetbrains.com>
2023-01-16 22:38:56 +0400

    Update BaseEntitySubscriber

--- shared/homeless/src/EventSubscriber/BaseEntitySubscriber.php
+++ shared/homeless/src/EventSubscriber/BaseEntitySubscriber.php
@@ -4,10 +4,12 @@ namespace App\EventSubscriber;
 
 use App\Entity\BaseEntityInterface;
 use App\Entity\User;
+use DateTime;
 use Doctrine\Common\EventSubscriber;
-use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
@@ -16,16 +18,17 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
  * Устанавливает для создаваемой/изменяемой сущности значения:
  * когда создано, кем создано, когда изменено, кем изменено
  */
+#[AutoconfigureTag(name: 'doctrine.event_subscriber', attributes: ['connection' => 'default'])]
 class BaseEntitySubscriber implements EventSubscriber
 {
-    private $tokenStorage;
+    private TokenStorageInterface $tokenStorage;
 
     public function __construct(TokenStorageInterface $tokenStorage)
     {
         $this->tokenStorage = $tokenStorage;
     }
 
-    public function getSubscribedEvents()
+    public function getSubscribedEvents(): array
     {
         return array(
             Events::prePersist,
@@ -35,12 +38,12 @@ class BaseEntitySubscriber implements EventSubscriber
 
     public function prePersist(LifecycleEventArgs $args)
     {
-        $entity = $args->getEntity();
+        $entity = $args->getObject();
         $user = $this->getUser();
 
         if ($entity instanceof BaseEntityInterface) {
             if (empty($entity->getCreatedAt())) {
-                $entity->setCreatedAt(new \DateTime());
+                $entity->setCreatedAt(new DateTime());
             }
 
             if (empty($entity->getCreatedBy())) {
@@ -51,16 +54,16 @@ class BaseEntitySubscriber implements EventSubscriber
 
     public function preUpdate(PreUpdateEventArgs $args)
     {
-        $entity = $args->getEntity();
+        $entity = $args->getObject();
         $user = $this->getUser();
 
         if ($entity instanceof BaseEntityInterface) {
-            $entity->setUpdatedAt(new \DateTime());
+            $entity->setUpdatedAt(new DateTime());
             $entity->setUpdatedBy($user);
         }
     }
 
-    public function getUser()
+    public function getUser(): ?User
     {
         $token = $this->tokenStorage->getToken();
 
