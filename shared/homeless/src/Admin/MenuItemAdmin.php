446670bd2d6cabb24a32af4b5c5d3525f38d95a6
Semyon.Atamas <semyon.atamas@jetbrains.com>
2023-02-13 23:27:41 +0400

    Add show action to MenuItem

--- shared/homeless/src/Admin/MenuItemAdmin.php
+++ shared/homeless/src/Admin/MenuItemAdmin.php
@@ -2,20 +2,18 @@
 
 namespace App\Admin;
 
-use App\Controller\ClientController;
-use App\Entity\Client;
 use App\Entity\MenuItem;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
 #[AutoconfigureTag(name: 'sonata.admin', attributes: [
     'manager_type' => 'orm',
-    'label' => 'Отображение разделов',
+    'label' => 'render_menu_item',
     'model_class' => MenuItem::class,
     'label_translator_strategy' => 'sonata.admin.label.strategy.underscore'
 ])]
-
 class MenuItemAdmin extends BaseAdmin
 {
     protected array $datagridValues = array(
@@ -31,6 +29,23 @@ class MenuItemAdmin extends BaseAdmin
     protected function configureFormFields(FormMapper $form): void
     {
         $form
+            ->add('name', null, [
+                'label' => 'Название',
+                'required' => true,
+            ])
+            ->add('code', null, [
+                'label' => 'Код',
+                'required' => true,
+            ])
+            ->add('enabled', null, [
+                'label' => 'Включен',
+                'required' => false,
+            ]);
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
             ->add('name', null, [
                 'label' => 'Название',
                 'required' => true,
