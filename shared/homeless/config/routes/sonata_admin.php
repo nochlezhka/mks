e639d31d5fba9546aa5fc8e1f8bf5f5bf59e0d4e
Alexander Menshchikov <alexander.menshchikov@yandex.ru>
2023-04-04 00:06:11 +0300

    Enable opcache + fix root page redirect

--- shared/homeless/config/routes/sonata_admin.php
+++ shared/homeless/config/routes/sonata_admin.php
@@ -6,19 +6,19 @@ namespace Symfony\Component\Routing\Loader\Configurator;
 use Symfony\Bundle\FrameworkBundle\Controller\RedirectController;
 
 return static function (RoutingConfigurator $routes): void {
-    $routes->import('@SonataAdminBundle/Resources/config/routing/sonata_admin.xml');
-    $routes->import('.', 'sonata_admin');
-
-    $routes->import('@SonataUserBundle/Resources/config/routing/admin_security.xml');
-    $routes->import('@SonataUserBundle/Resources/config/routing/admin_resetting.xml')
-        ->prefix('/resetting')
-    ;
-
     $routes->add('root', '/dashboard')
         ->controller(RedirectController::class)
         ->defaults([
             'route' => 'my_clients',
             'permanent' => true,
         ])
+    ;
+
+    $routes->import('@SonataAdminBundle/Resources/config/routing/sonata_admin.xml');
+    $routes->import('.', 'sonata_admin');
+
+    $routes->import('@SonataUserBundle/Resources/config/routing/admin_security.xml');
+    $routes->import('@SonataUserBundle/Resources/config/routing/admin_resetting.xml')
+        ->prefix('/resetting')
     ;
 };
